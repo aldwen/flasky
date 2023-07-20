b7a6d641650c35291a9ab75d0d1bcf654c26a561
wenwen_ubuntu2204_wsl_wenworkra <no@qq.com>
2023-07-20 18:28:39 +0800

    finished the job in chapter 2

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask import make_response
+from flask import redirect
+from flask import url_for
+from flask import abort
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +13,17 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return f"<h1>Hello, {name}!</h1><img src='http://helloflask.com/totoro.gif'>"
+
+@app.route('/cookie')
+def cookie():
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer','42')
+    return response
+
+@app.route('/redirect')
+def movetoindex():
+    return redirect(url_for('index'))
+
+@app.route('/aaa')
+def get_user():
+    abort(403)
