8eb0a29b55bfaa7d91b24ef793f665d62bc02546
wenwen_ubuntu2204_wsl_wenworkra <no@qq.com>
2023-07-20 22:29:49 +0800

    finieshed 3.5

--- hello.py
+++ hello.py
@@ -4,8 +4,10 @@ from flask import redirect
 from flask import url_for
 from flask import abort
 from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -28,3 +30,11 @@ def movetoindex():
 @app.route('/aaa')
 def get_user():
     abort(403)
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
