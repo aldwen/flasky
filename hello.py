86f3ee12f55651883ab99c9bf99ef24aca6018b5
wenwen_ubuntu2204_wsl_wenworkra <no@qq.com>
2023-07-26 16:00:33 +0800

    finished chapter 4

--- hello.py
+++ hello.py
@@ -4,26 +4,39 @@ from flask import redirect
 from flask import url_for
 from flask import abort
 from flask import render_template
+from flask import session
+from flask import flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
 moment= Moment(app)
 
-app.config['SECRET_KEY'] = 'WHEN YOU SEE WOULD DIE'
+app.config['WTF_CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = 'WHEN YOU SEE YOU WOULD DIE'
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators = [DataRequired()])
-    submit = SubmitField ('Submit...')
+    submit = SubmitField ('提交')
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name= session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('看起来你改了名字')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    else:
+        return render_template('index.html',form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
